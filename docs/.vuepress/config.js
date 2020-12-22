c4817a01a2a51eddb0c4371c69411f7bac454207
fanqing <fanqing@enmonster.com>
2020-12-22 11:57:36 +0800

    提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,43 +1,49 @@
 module.exports = {
-  title: "FFBLOG", // 设置网站标题
-  description: "FF的博客",
-  themeConfig: {
-    nav: [
-      { text: "主页", link: "/" },
-      {
-        text: "博文",
-        items: [
-          { text: "Android", link: "/android/" },
-          { text: "Ios", link: "/ios/" },
-        ],
-      },
-
-      { text: "关于", link: "/about/" },
-      { text: "Github", link: "https://github.com/a674125938/myBlog" },
+    title: '首页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+    description: '前端记录', // meta 中的描述文字，用于SEO
+    // 注入到当前页面的 HTML <head> 中的标签
+    head: [
+        ['link', { rel: 'icon', href: '/egg.png' }],  //浏览器的标签栏的网页图标
     ],
-    sidebar: {
-      "/": [
-        {
-          title: "Group 1", // 必要的
-          path: "/", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-          collapsable: false, // 可选的, 默认值是 true,
-          sidebarDepth: 1, // 可选的, 默认值是 1
-          children: [
-            "apiword", //根目录创建 apiword.md文件
-            "api", //根目录创建 api.md文件
-            "error", //根目录创建 error.md文件
-          ],
-        },
-        {
-          title: "Group 2",
-          children: ["web1"],
-          initialOpenGroupIndex: -1, // 可选的, 默认值是 0
-        },
-      ],
-      "/android/": ["android1"],
-      "/ios/": ["ios1"],
-      "/about/": ["about1"],
+    markdown: {
+        lineNumbers: true
     },
-    sidebarDepth: 2,
-  },
-};
+    serviceWorker: true,
+    themeConfig: {    
+        logo: '/egg.png',
+        lastUpdated: 'lastUpdate', // string | boolean
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '分类',
+                ariaLabel: '分类',
+                items: [
+                    { text: '文章', link: '/pages/folder1/test1.md' },
+                    { text: '琐碎', link: '/pages/folder2/test4.md' },
+                ]
+            },
+            { text: '功能演示', link: '/pages/folder1/test3.md' },
+            { text: 'Github', link: 'https://github.com/a674125938' },
+        ],
+        sidebar: {
+            '/pages/folder1/':[
+                {
+                    title: '测试菜单1',   // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    children: [
+                        ['test1.md', '子菜单1'],
+                        ['test3.md', '子菜单2']
+                    ]
+                },
+                {
+                    title: '测试菜单2',
+                    collapsable: false, // 可选的, 默认值是 true,
+                    children: [
+                        ['test2.md', '子菜单1']
+                    ]
+                }
+            ],
+        }
+    }
+}
